a5277312957aad2f3de1e1ef7996960ccf1ade8e
unknown <ngotienpts@gmail.com>
2022-07-26 17:10:23 +0700

    26/7

--- js/main.js
+++ js/main.js
@@ -12,4 +12,35 @@ closeSubMenu.onclick = function() {
 };
 // end show sub menu
 
+// show edit form
+var closeEditForms = document.querySelectorAll('.close_edit');
+var editForms = document.querySelectorAll('.js_click_edit');
+if(closeEditForms){
+    closeEditForms.forEach((el)=>{
+        el.onclick = function(){
+            if(!el.closest('.is_hide')){
+                el.parentElement.classList.add('is_hide')
+            }
+        }
+    }
+    )
+}
 
+if(editForms){
+    editForms.forEach((el)=>{
+        el.onclick = function(){
+            var showEdit = el.parentElement.querySelector('.box_edit_form');
+            if(showEdit.classList.contains('is_hide')){
+                showEdit.classList.remove('is_hide')
+            }else {
+                showEdit.classList.add('is_hide')
+            }
+        }
+    })
+}
+
+// change avaater
+var getImg = document.querySelector('.getFile');
+getImg.onclick = function(){
+    console.log(getImg.value);
+}
